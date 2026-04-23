96efe554fe35a422cf252c7082ba062f1edacafe
PolinaStarikova06 <p.starikova06@icloud.com>
2026-04-23 18:26:25 +0500

    Add files via upload

---
+++ 9long.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githack.com/PolinaStarikova06/2804/main/%D1%84%D0%BE%D1%82%D0%BE1.jpg",
+
+  "https://raw.githack.com/PolinaStarikova06/2804/main/%D1%84%D0%BE%D1%82%D0%BE2.jpg",
+
+  "https://raw.githack.com/PolinaStarikova06/2804/main/%D1%84%D0%BE%D1%82%D0%BE3.jpg",
+
+  "https://raw.githack.com/PolinaStarikova06/2804/main/%D1%84%D0%BE%D1%82%D0%BE7.jpg",
+
+  "https://raw.githack.com/PolinaStarikova06/2804/main/%D1%84%D0%BE%D1%82%D0%BE5.png",
+
+  "https://raw.githack.com/PolinaStarikova06/2804/main/%D1%84%D0%BE%D1%82%D0%BE6.jpg"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "EVERYTHING ALRIGHT",
+
+  "SLEEPING BEAUTY",
+
+  "THE ZOO",
+
+  "CUT IT OUT",
+
+  "WE TRUST",
+
+  "PITY, BUT NOT"
+
+];
